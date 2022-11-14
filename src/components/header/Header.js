1f6caed00b9a2decda599defb9af2d6a8328b166
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-11-14 22:11:17 +0530

    nav menu style enhancement

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,25 +2,24 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import "./Header.css";
+import './Header.css';
 export default function Header() {
   return (
     <header>
-       <Navbar bg="dark" expand="lg" className="header">
-        <Container className="contabar">
-          <Navbar.Brand href="#home">Salyaka</Navbar.Brand>
+      <Navbar bg="dark" expand="lg" className="header">
+        <Container>
+          <Navbar.Brand href="#home">Slyaka </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+            <Nav className="me-auto contabar nav-item">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="/about">About Us</Nav.Link>
               <Nav.Link href="#link">Services</Nav.Link>
               <Nav.Link href="#link">Pricing</Nav.Link>
-              
             </Nav>
           </Navbar.Collapse>
         </Container>
-      </Navbar>   
+      </Navbar>
     </header>
   );
 }
