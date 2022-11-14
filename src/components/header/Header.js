5c46e18ef0ee537f92443de734c14e1a118ae9ae
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-11-14 22:27:07 +0530

    header component style enhancement

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,11 +11,19 @@ export default function Header() {
           <Navbar.Brand href="#home">Slyaka </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto contabar nav-item">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="/about">About Us</Nav.Link>
-              <Nav.Link href="#link">Services</Nav.Link>
-              <Nav.Link href="#link">Pricing</Nav.Link>
+            <Nav className="me-auto contabar nav-items">
+              <Nav.Link href="#home" className="nav-item">
+                Home
+              </Nav.Link>
+              <Nav.Link href="/about" className="nav-item">
+                About Us
+              </Nav.Link>
+              <Nav.Link href="#link" className="nav-item">
+                Services
+              </Nav.Link>
+              <Nav.Link href="#link2" className="nav-item">
+                Pricing
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
