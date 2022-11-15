48f59fa479a7738ab274b0a589ba0f785dbf823d
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-11-15 09:42:06 +0530

    2 files modified

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,12 +3,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './Header.css';
+import Carousel from 'react-bootstrap/Carousel';
+
 export default function Header() {
   return (
     <header>
       <Navbar bg="dark" expand="lg" className="header">
-        <Container>
-          <Navbar.Brand href="#home">Slyaka </Navbar.Brand>
+        <Container >
+          <Navbar.Brand href="#home" className="homey">Slyaka </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto contabar nav-items">
@@ -28,6 +30,44 @@ export default function Header() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      
+      <Carousel className="slide">
+      <Carousel.Item interval={1000}>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
     </header>
   );
 }
