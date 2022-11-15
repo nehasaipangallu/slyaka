c857b68014b02ed2eacef88b043cef7c49c918b8
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-11-15 18:34:58 +0530

    1 file modified

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -19,14 +19,45 @@ export default function About() {
         professional minds. We are available for hire in a wide range of
         creative displaces for a variety of jobs, projects, and gigs.</p>
         We are the best specialized in following
-<p><b>+ web development
+<p><div class="accordion accordion-flush" id="accordionFlushExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingOne">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+        Accordion Item #2
+      </button>
+    </h2>
+    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+        Accordion Item #3
+      </button>
+    </h2>
+    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+    </div>
+  </div>
+</div>+ web development
 + Mobile app development
 + Android app development
 + iOS app development
 + Desktop application and development
 + Software development
 + Digital marketing and branding
-+ IT consultingp</b></p>
++ IT consultingp</p>
 <p>We develop innovative and artistic products and services that provide total communication and knowledge solutions.
 Our business philosophy is to confirm the very greatest top quality, total client satisfaction, timely delivery, and therefore the highest quality. We convert client requirements into an answer that specializes in building secure, scalable, and centric products.
 We are having a creative and focused company staff. Extend your team with our experts, and follow the proper development methodologies practices, and work culture.
