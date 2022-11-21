4caffa0db267d8f9e012a05d738e9e5582aeb878
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-11-21 15:06:57 +0530

    Aboutus page styling

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -31,8 +31,8 @@ export default function About() {
         <p class="text-start text-break">
           We are the best specialized in following
         </p>
+        <br/>
         <div class="row">
-         
           <img
             src="https://github.com/nehasaipangallu/slyaka/blob/main/assets/undraw_web_developer_re_h7ie.svg?raw=true"
             class="col-lg-4 col-md-6 col-sm-6 col-6"
@@ -40,8 +40,8 @@ export default function About() {
           <p class="col-lg-8 col-md-6 col-sm-12 img-cap">
             {' '}
             <h5>
-            <strong>web development</strong>
-          </h5>
+              <strong>web development</strong>
+            </h5>
             we are the simplest dedicated in web designing & development,
             android apps, digital marketing in India. We are the simplest
             leading company, web designing development, android application apps
@@ -52,29 +52,97 @@ export default function About() {
             custom web application to satisfy your company’s business needs.
           </p>
         </div>
-        <div class="row">
        
+        <div class="row">
           <p class="col-lg-8 col-md-6 col-sm-12 img-cap">
-          <h5 class="col-lg-8 col-md-6 col-sm-12">
-            <strong>Mobile App Development</strong>
-          </h5>
-          Mobile app development is a process for building mobile applications that run on mobile devices. These applications can either be pre-installed or downloaded and installed by the user later. They use the network capabilities of the device to work computing resources remotely.  Hence, the mobile app development process requires creating software that can be installed on the device, and enabling backend services for data access through APIs, and testing the application on target devices.
-            </p>
-            <img
-            src="https://github.com/nehasaipangallu/slyaka/blob/main/assets/undraw_mobile_apps_re_3wjf.svg?raw=true" class="col-lg-4 col-md-6 col-sm-6 col-6"/>
-          </div>
-          
-          <div class="row">
-         
-         <img src="https://github.com/nehasaipangallu/slyaka/blob/main/assets/undraw_mobile_development_re_wwsn.svg?raw=true" class="col-lg-4 col-md-6 col-sm-6 col-6"/>
-         <p class="col-lg-8 col-md-6 col-sm-12 img-cap">
-          <h5 class="col-lg-8 col-md-6 col-sm-12">
-            <strong>ios App Development</strong>
-          </h5>
-          iOS application development is the process of making mobile applications for Apple hardware, including iPhone, iPad and iPod Touch. The software is written in the Swift programming language or Objective-C and then deployed to the App Store for users to download.
-            </p>
-         
-         </div>
+            <h5 class="col-lg-8 col-md-6 col-sm-12">
+              <strong>Android App Development</strong>
+            </h5>
+            Android app development is a process for building mobile
+            applications that run on mobile devices. These applications can
+            either be pre-installed or downloaded and installed by the user
+            later. They use the network capabilities of the device to work
+            computing resources remotely. Hence, the android app development
+            process requires creating software that can be installed on the
+            device, and enabling backend services for data access through APIs,
+            and testing the application on target devices.
+          </p>
+          <img
+            src="https://github.com/nehasaipangallu/slyaka/blob/main/assets/undraw_mobile_apps_re_3wjf.svg?raw=true"
+            class="col-lg-4 col-md-6 col-sm-6 col-6"
+          />
+        </div>
+       
+
+        <div class="row">
+          <img
+            src="https://github.com/nehasaipangallu/slyaka/blob/main/assets/undraw_mobile_development_re_wwsn.svg?raw=true"
+            class="col-lg-4 col-md-6 col-sm-6 col-6"
+          />
+          <p class="col-lg-8 col-md-6 col-sm-12 img-cap">
+            <h5 class="col-lg-8 col-md-6 col-sm-12">
+              <strong>ios App Development</strong>
+            </h5>
+            iOS application development is the process of making mobile
+            applications for Apple hardware, including iPhone, iPad and iPod
+            Touch. The software is written in the Swift programming language or
+            Objective-C and then deployed to the App Store for users to
+            download.
+          </p>
+        </div>
+
+        <div class="row">
+          <p class="col-lg-8 col-md-6 col-sm-12 img-cap">
+            <h5 class="col-lg-8 col-md-6 col-sm-12">
+              <strong>software Development</strong>
+            </h5>
+            Software development refers to a set of computer science activities
+            dedicated to the process of creating, designing, deploying and
+            supporting software. Software itself is the set of instructions or
+            programs that tell a computer what to do. It is independent of
+            hardware and makes computers programmable.<b> There are three basic
+            types:</b><br/>System software to provide core functions such as operating
+            systems, disk management, utilities, hardware management and other
+            operational necessities. Programming software to give programmers
+            tools such as text editors, compilers, linkers, debuggers and other
+            tools to create code. Application software (applications or apps) to
+            help users perform tasks. Office productivity suites, data
+            management software, media players and security programs are
+            examples. Applications also refers to web and mobile applications
+            like those used to shop on Amazon.com, socialize with Facebook or
+            post pictures to Instagram.
+          </p>
+          <img
+            src="https://github.com/nehasaipangallu/slyaka/blob/main/assets/undraw_web_devices_re_m8sc.svg?raw=true"
+            class="col-lg-4 col-md-6 col-sm-6 col-6"
+          />
+        </div>
+        <br/>
+        <div class="row">
+          <img
+            src="https://github.com/nehasaipangallu/slyaka/blob/main/assets/undraw_mobile_marketing_re_p77p.svg?raw=true"
+            class="col-lg-4 col-md-6 col-sm-6 col-6"
+          />
+          <p class="col-lg-8 col-md-6 col-sm-12 img-cap">
+            <h5 class="col-lg-8 col-md-6 col-sm-12">
+              <strong>Digital Marketing</strong>
+            </h5>
+            Any marketing that uses electronic devices and can be used by marketing specialists to convey promotional messaging and measure its impact through your customer journey. In practice, digital marketing typically refers to marketing campaigns that appear on a computer, phone, tablet, or other device. It can take many forms, including online video, display ads, search engine marketing, paid social ads and social media posts.
+          </p>
+        </div>
+        <div class="row">
+          <p class="col-lg-8 col-md-6 col-sm-12 img-cap">
+            <h5 class="col-lg-8 col-md-6 col-sm-12">
+              <strong>It Consulting</strong>
+            </h5>
+            IT consulting services are advisory services that help clients assess different technology strategies and, in doing so, align their technology strategies with their business or process strategies. These services support customers’ IT initiatives by providing strategic, architectural, operational and implementation planning. Strategic planning includes advisory services that help clients assess their IT needs and formulate system implementation plans. Architecture planning includes advisory services that combine strategic plans and knowledge of emerging technologies to create the logical design of the system and the supporting infrastructure to meet customer requirements.
+          </p>
+          <img
+            src="https://github.com/nehasaipangallu/slyaka/blob/main/assets/undraw_development_re_g5hq.svg?raw=true"
+            class="col-lg-4 col-md-6 col-sm-6 col-6"
+          />
+        </div>
+
         {/* <Accordion>
           <Accordion.Item eventKey="0">
             <Accordion.Header> web development</Accordion.Header>
